82352e610ab1a1ed1ceb4732ac41e496a6cddc8a
vitoriakamili <00001089832709sp@al.educacao.sp.gov.br>
2024-11-12 17:50:06 -0300

    Add files via upload

---
+++ segundo_trabalho/script.js
@@ -0,0 +1,6 @@
+// Função para alternar a visibilidade da sidebar
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open');
+}
+    
